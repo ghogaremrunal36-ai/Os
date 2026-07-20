ae6b2c41fdeec4021a7ca3c5cbdb74b190790dd8
ghogaremrunal36-ai <ghogaremrunal36@gmail.com>
2026-07-20 14:29:30 +0530

    Add files via upload

---
+++ practical 3.1.py
@@ -0,0 +1,47 @@
+import threading
+import time
+
+
+def task(name):
+    print(f"{name} started")
+    time.sleep(2)      
+    print(f"{name} finished")
+
+
+print("\n--- Sequential Execution ---")
+
+start_time = time.time()
+
+task("Task 1")
+task("Task 2")
+task("Task 3")
+task("Task 4")
+
+end_time = time.time()
+
+print("Sequential Execution Time:",
+      round(end_time - start_time, 2), "seconds")
+
+
+print("\n--- Threaded Execution ---")
+
+start_time = time.time()
+
+threads = []
+
+for i in range(1, 5):
+    t = threading.Thread(target=task, args=(f"Task {i}",))
+    threads.append(t)
+
+
+for t in threads:
+    t.start()
+
+
+for t in threads:
+    t.join()
+
+end_time = time.time()
+
+print("Threaded Execution Time:",
+      round(end_time - start_time, 2), "seconds")
